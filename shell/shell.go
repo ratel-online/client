e5dccb895541fc8339eb09551fa8d100ec05d97f
Nico <ainililia@163.com>
2021-12-01 20:35:42 +0800

    is single

--- shell/shell.go
+++ shell/shell.go
@@ -1,51 +1,54 @@
 package shell
 
 import (
-    "fmt"
-    "github.com/howeyc/gopass"
-    "github.com/ratel-online/client/api"
-    "github.com/ratel-online/client/ctx"
-    "github.com/ratel-online/client/util"
-    "github.com/ratel-online/core/log"
+	"github.com/ratel-online/client/ctx"
+	"github.com/ratel-online/client/model"
+	"github.com/ratel-online/core/log"
 )
 
 type shell struct {
-    ctx  *ctx.Context
-    addr string
+	ctx  *ctx.Context
+	addr string
 }
 
 func New(addr string) *shell {
-    return &shell{
-        addr: addr,
-    }
+	return &shell{
+		addr: addr,
+	}
 }
 
 func (s *shell) Start() error {
-    fmt.Printf("usr: ")
-    username, err := util.Readline()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("pwd: ")
-    password, err := gopass.GetPasswd()
-    if err != nil {
-        panic(err)
-    }
-    resp, err := api.Login(string(username), string(password))
-    if err != nil {
-        log.Error(err)
-        return err
-    }
-    s.ctx = ctx.New(resp.Data)
-    err = s.ctx.Connect("tcp", s.addr)
-    if err != nil {
-        log.Error(err)
-        return err
-    }
-    err = s.ctx.Auth()
-    if err != nil {
-        log.Error(err)
-        return err
-    }
-    return s.ctx.Listener()
+	//fmt.Printf("usr: ")
+	//username, err := util.Readline()
+	//if err != nil {
+	//    panic(err)
+	//}
+	//fmt.Printf("pwd: ")
+	//password, err := gopass.GetPasswd()
+	//if err != nil {
+	//    panic(err)
+	//}
+	//resp, err := api.Login(string(username), string(password))
+	//if err != nil {
+	//    log.Error(err)
+	//    return err
+	//}
+	s.ctx = ctx.New(model.LoginRespData{
+		ID:       1,
+		Name:     "Nico",
+		Score:    100,
+		Username: "Nico",
+		Token:    "aeiou",
+	})
+	err := s.ctx.Connect("tcp", s.addr)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	err = s.ctx.Auth()
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	return s.ctx.Listener()
 }
