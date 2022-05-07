ebb685b9b8177f68f8510ee4d3c0bd2d510fe014
EldersJavas <eldersjavas@gmail.com>
2022-05-07 18:30:06 +0800

    feat✨:  add -help

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ratel-online/client/shell"
 	"github.com/ratel-online/core/log"
 )
 
 func main() {
+	if len(os.Args) > 0 {
+        for _, arg := range os.Args {
+            if arg=="-help" {
+				fmt.Print(HELP)
+				os.Exit(0)
+			}
+        }
+    }
 	log.Error(shell.New("127.0.0.1:9999").Start())
 }
